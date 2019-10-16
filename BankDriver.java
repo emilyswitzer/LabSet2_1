ff3972f763e9da5b8371720f08ef7e77fb565117
emilyswitzer <55185998+emilyswitzer@users.noreply.github.com>
2019-10-16 11:01:20 +0100

    Add files via upload

---
+++ BankDriver.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class BankDriver {
+    public static void main(String[] args) {
+        ModifiedBankAccount one = new ModifiedBankAccount();
+        String choice = JOptionPane.showInputDialog("Please enter amount you would like to"
+        +"Lodge or withdraw");
+
+        }
+
+    }
+}
